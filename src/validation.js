6254fa23fefd909d31263b4979dea613d1fbae64
nishchay271095 <nishchay271095@gmail.com>
2020-10-15 11:58:37 +0530

    Replace hap/joi with Joi as per the latest documentation

--- src/validation.js
+++ src/validation.js
@@ -1,9 +1,21 @@
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 
 const userValidator = (data) => {
     const schema = Joi.object(
         {
-            username: Joi.string().required()
+            username: Joi.string().required().min(5).error(errors=>{
+                errors.forEach(err=>{
+                    if(err.code == "string.empty")
+                    {
+                        err.message = "Username is required";
+                    }
+                    else if(err.code == "string.min")
+                    {
+                        err.message = "Minimum length of username is 5"
+                    }
+                });
+                return errors;
+            })
         }
     );
     return schema.validate(data);
